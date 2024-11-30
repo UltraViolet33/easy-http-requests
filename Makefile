14027549719142133c23950fabc469699f1b42f8
UltraViolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-11-30 13:39:25 +0100

    add license, black formating pre-commit and test coverage

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 test:
 	poetry run pytest tests
+
+test-coverage:
+	poetry run pytest --cov=easy_http_requests
+
+docs:
+	cd docs && make html
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'esay-http-requests'
-copyright = '2024, UltraViolet33'
-author = 'UltraViolet33'
-release = '0.0.1'
+project = "esay-http-requests"
+copyright = "2024, UltraViolet33"
+author = "UltraViolet33"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
