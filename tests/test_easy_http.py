34db74a281e6b80d008ffabb16f873388559fe43
UltraViolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-12-02 07:36:52 +0100

    improve tests

---
+++ tests/test_easy_http.py
@@ -0,0 +1,5 @@
+import unittest
+
+
+class TestEasyHttp(unittest.TestCase):
+    BASE_URL = "https://api.example.com"
--- tests/test_easy_http_request.py
+++ tests/test_easy_http_request.py
@@ -3,9 +3,10 @@ from requests.models import Response
 from easy_http_requests.easy_http_request import EasyHttpRequest
 from easy_http_requests.easy_http_response import EasyHttpResponse
 from requests.structures import CaseInsensitiveDict
+from tests.test_easy_http import TestEasyHttp
 
 
-class TestEasyHttpRequest:
+class TestEasyHttpRequest(TestEasyHttp):
 
     def test_create_easy_http_request(self):
         easy_http_request = EasyHttpRequest("base_url/")
@@ -28,7 +29,9 @@ class TestEasyHttpRequest:
         client = EasyHttpRequest(base_url="https://api.example.com")
         response = client.get("endpoint")
 
-        mock_request.assert_called_once_with("GET", "https://api.example.com/endpoint")
+        mock_request.assert_called_once_with(
+            "GET", "https://api.example.com/endpoint", params=None, data=None, json=None
+        )
         assert response.status_code == 200
         assert response.headers == {"Content-Type": "application/json"}
         assert response.body == {"key": "value"}
@@ -42,7 +45,9 @@ class TestEasyHttpRequest:
         client = EasyHttpRequest(base_url="https://api.example.com")
         response = client._make_request("GET", "endpoint")
 
-        mock_request.assert_called_once_with("GET", "https://api.example.com/endpoint")
+        mock_request.assert_called_once_with(
+            "GET", "https://api.example.com/endpoint", params=None, data=None, json=None
+        )
         assert response == mock_response
 
     @patch("requests.request")
@@ -56,43 +61,12 @@ class TestEasyHttpRequest:
         client = EasyHttpRequest()
         response = client.get("endpoint")
 
-        mock_request.assert_called_once_with("GET", "endpoint")
+        mock_request.assert_called_once_with(
+            "GET", "endpoint", params=None, data=None, json=None
+        )
         assert response.status_code == 200
         assert response.body == {"key": "value"}
         assert response.text == '{"key": "value"}'
-
-    @patch("requests.request")
-    def test_get_failure(self, mock_request):
-        mock_request.side_effect = Exception("HTTP request failed")
-
-        client = EasyHttpRequest(base_url="https://api.example.com")
-        try:
-            client.get("endpoint")
-        except Exception as e:
-            assert str(e) == "HTTP request failed"
-            assert isinstance(e, Exception)
-
-    @patch("requests.request")
-    def test_get_timeout(self, mock_request):
-        mock_request.side_effect = TimeoutError("The request timed out")
-
-        client = EasyHttpRequest(base_url="https://api.example.com")
-        try:
-            client.get("endpoint")
-        except TimeoutError as e:
-            assert str(e) == "The request timed out"
-            assert isinstance(e, TimeoutError)
-
-    @patch("requests.request")
-    def test_get_connection_error(self, mock_request):
-        mock_request.side_effect = ConnectionError("Failed to connect to the server")
-
-        client = EasyHttpRequest(base_url="https://api.example.com")
-        try:
-            client.get("endpoint")
-        except ConnectionError as e:
-            assert str(e) == "Failed to connect to the server"
-            assert isinstance(e, ConnectionError)
 
     @patch("requests.request")
     def test_easy_http_request_returns_easy_http_response(self, mock_request):
